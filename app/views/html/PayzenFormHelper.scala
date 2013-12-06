bfbfce4aa704b23e693c51fb80ae36ddb99f0ef8
Valentin Kasas <valentin.kasas@gmail.com>
2013-12-06 18:39:03 +0100

    pattern matching was unnecessary

--- app/views/html/PayzenFormHelper.scala
+++ app/views/html/PayzenFormHelper.scala
@@ -9,8 +9,7 @@ object PayzenFormHelper {
 
   def formFields(data: Map[String,String]): Html =
     data.toList.foldLeft(Html("")){
-      case (html, keyvalue)=>
-        html += Html(s"""<input type="hidden" name="${keyvalue._1}" value="${keyvalue._2}"/>""")
+      (html, keyvalue) => html += Html(s"""<input type="hidden" name="${keyvalue._1}" value="${keyvalue._2}"/>""")
     }
 
 }
